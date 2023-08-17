f882c8684b3492d97f86e55fe165643c4ce279a7
LGiki <qq547221432@gmail.com>
2023-08-17 17:28:19 +0800

    Completed help dialog

--- components/help-dialog.tsx
+++ components/help-dialog.tsx
@@ -9,34 +9,26 @@ export default function HelpDialog(props: {
             open={props.open}
             onOpenChange={props.onOpenChange}
         >
-            <DialogContent className='max-w-2xl'>
+            <DialogContent className='rounded-xl max-h-screen overflow-y-auto text-justify'>
                 <DialogHeader>
                     <DialogTitle>Learn More</DialogTitle>
                 </DialogHeader>
-                <div className='flex flex-col gap-2'>
-                    <h1 className='text-xl'>Help:</h1>
-                    <img src='/fe-color-matrix.svg' />
-                    <h1 className='text-xl'>Docs:</h1>
-                    <ul className='list-disc'>
-                        <li>
-                            <a
-                                target='_blank'
-                                className='underline underline-offset-4'
-                                href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix"
-                            >
-                                &lt;feColorMatrix&gt; on MDN
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                target='_blank'
-                                className='underline underline-offset-4'
-                                href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix"
-                            >
-                                &lt;feColorMatrix&gt; on MDN
-                            </a>
-                        </li>
-                    </ul>
+                <div className='flex flex-col gap-2 overflow-y-auto'>
+                    <p>The &lt;feColorMatrix&gt; filter changes colors based on a 4 x 5 transformation matrix.</p>
+                    <img src='/fe-color-matrix.svg' width={180} alt='feColorMatrix'/>
+                    <p>Every pixel&apos;s color value (represented by an [R,G,B,A] vector) is matrix multiplied to
+                        create a new color.</p>
+                    <p>Assuming [R&apos;, G&apos;, B&apos;, A&apos;] represents the transformed pixel, the operation of
+                        feColorMatrix can be expressed as follows:</p>
+                    <img src='/fe-color-matrix-equation.svg' alt='feColorMatrix equation'/>
+                    <p>Read more about feColorMatrix on <a
+                        target='_blank'
+                        className='underline underline-offset-4'
+                        href="https://developer.mozilla.org/en-US/docs/Web/SVG/Element/feColorMatrix"
+                    >
+                        MDN
+                    </a>.
+                    </p>
                 </div>
             </DialogContent>
         </Dialog>
